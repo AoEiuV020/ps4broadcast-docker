6822084704b72626fa9ece669ff0d1d9a27d66c3
AoEiuV020 <aoeiuv020@gmail.com>
2024-05-15 21:45:10 +0800

    添加ci自动打包发布

--- script/env.sh
+++ script/env.sh
@@ -2,6 +2,6 @@
 set -e
 script_dir=$(cd $(dirname $0);pwd)
 ROOT=$(dirname $script_dir)
-IMAGE_NAME=ps4broadcast
-CONTAINER_NAME=ps4b
-DOCKER_ID=aoeiuv020
+IMAGE_NAME="${IMAGE_NAME:-ps4broadcast}"
+CONTAINER_NAME="${CONTAINER_NAME:-ps4b}"
+DOCKER_ID="${DOCKER_ID:-aoeiuv020}"
